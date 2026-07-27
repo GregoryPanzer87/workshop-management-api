00bd4d9a637829bc09ebeaeda767006717b49fd3
Gregory Guedez <gregorypaul2011@hotmail.com>
2026-07-27 15:30:27 -0400

    feat(auth): implement JWT validation and permission checking

--- app/api/deps.py
+++ app/api/deps.py
@@ -9,15 +9,10 @@ from app.core.security import SECRET_KEY, ALGORITHM
 from app.crud import crud_user
 from app.database import SessionLocal
 from app.models import User
+from app.database import get_db
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
-def get_db() -> Generator[Session, None, None]:
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 def get_current_user(
     db: Session = Depends(get_db), 
@@ -30,11 +25,34 @@ def get_current_user(
     )
 
     try:
-        payload = dict(jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM]))
-        
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         user_id: str = payload.get("sub")
         if user_id is None:
             raise credentials_exception
             
     except (jwt.PyJWTError, ValidationError):
         raise credentials_exception
+
+    user = crud_user.get(db, id=int(user_id))
+    if not user:
+        raise credentials_exception
+    if not user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="Usuario inactivo"
+        )
+
+    return user
+    
+def require_max_permission_level(max_level: int):
+    """
+    Allows access according to user.permission.
+    """
+    def permission_checker(current_user: User = Depends(get_current_user)):
+        if current_user.permission > max_level:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="No tienes los permisos suficientes para esta acción"
+            )
+        return current_user
+    return permission_checker
